1dba92bc4ad68d8048b97e4b11b9e4d48985c486
Ogo Joshua <156670159+Ogojoshua905@users.noreply.github.com>
2024-08-24 18:11:34 +0100

    Update models.py
    Made changes

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     first_name = models.CharField(max_length=30, blank=False, null=False)
     last_name = models.CharField(max_length=30, blank=False, null=False)
-    role = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('member', 'Member')], null=True, blank=True)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
